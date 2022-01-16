e82b3db4fe9126c3d1f5bf2433fa1a06e24a0afe
Linux Hooligans <linuxhooligans@yandex.ru>
2022-01-16 16:17:44 +0300

    1

--- main.tf
+++ main.tf
@@ -22,6 +22,17 @@ resource "hcloud_network_subnet" "network-subnet-management" {
   ip_range     = "10.10.0.0/24"
 }
 
+resource "hcloud_network" "network-intranet-1" {
+  name     = "intranet-1"
+  ip_range = "10.100.0.0/16"
+}
+resource "hcloud_network_subnet" "network-subnet-intranet-1" {
+  type         = "cloud"
+  network_id   = hcloud_network.network-intranet-1.id
+  network_zone = "eu-central"
+  ip_range     = "10.100.0.0/24"
+}
+
 resource "hcloud_server" "node" {
   count       = 3
   name        = "node${count.index + 1}"
@@ -34,4 +45,10 @@ resource "hcloud_server_network" "network-management" {
   server_id  = hcloud_server.node[count.index].id
   network_id = hcloud_network.network-management.id
   ip         = "10.10.0.${count.index + 10}"
+}
+resource "hcloud_server_network" "network-intranet-1" {
+  count       = 3
+  server_id  = hcloud_server.node[count.index].id
+  network_id = hcloud_network.network-intranet-1.id
+  ip         = "10.100.0.${count.index + 10}"
 }
