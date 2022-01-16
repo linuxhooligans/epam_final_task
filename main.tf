95dbcbad5507597651a5831e8bf6508b4c538a45
Linux Hooligans <linuxhooligans@yandex.ru>
2022-01-16 13:53:00 +0300

    Test

--- main.tf
+++ main.tf
@@ -11,6 +11,20 @@ provider "hcloud" {
   token = var.hcloud_token
 }
 
+resource "hcloud_network" "network" {
+  name     = "management"
+  ip_range = "10.10.10.0/24"
+}
+resource "hcloud_network" "network" {
+  name     = "intranet_1"
+  ip_range = "10.100.0.0/24"
+}
+resource "hcloud_network" "network" {
+  name     = "intranet_2"
+  ip_range = "10.200.0.0/24"
+}
+
+
 resource "hcloud_server" "node" {
   count       = 3
   name        = "node${count.index}"
