e1d21e39dc523798d5ac3a35460fe099081712f9
Linux Hooligans <linuxhooligans@yandex.ru>
2022-01-16 14:25:30 +0300

    1

--- main.tf
+++ main.tf
@@ -11,28 +11,37 @@ provider "hcloud" {
   token = var.hcloud_token
 }
 
-resource "hcloud_network" "network" {
+resource "hcloud_network" "network-management" {
   name     = "management"
   ip_range = "10.10.0.0/16"
 }
-
 resource "hcloud_network_subnet" "network-subnet-management" {
   type         = "cloud"
-  network_id   = hcloud_network.network.id
+  network_id   = hcloud_network.network-management.id
   network_zone = "eu-central"
-  ip_range     = "10.10.10.0/24"
+  ip_range     = "10.10.0.0/24"
+}
+
+resource "hcloud_network" "network-intranet-1" {
+  name     = "intranet-1"
+  ip_range = "10.100.0.0/16"
 }
 resource "hcloud_network_subnet" "network-subnet-intranet-1" {
   type         = "cloud"
-  network_id   = hcloud_network.network.id
+  network_id   = hcloud_network.network-intranet-1.id
   network_zone = "eu-central"
-  ip_range     = "10.10.100.0/24"
+  ip_range     = "10.100.0.0/24"
+}
+
+resource "hcloud_network" "network-intranet-2" {
+  name     = "intranet-2"
+  ip_range = "10.200.0.0/16"
 }
 resource "hcloud_network_subnet" "network-subnet-intranet-2" {
   type         = "cloud"
-  network_id   = hcloud_network.network.id
+  network_id   = hcloud_network.network-intranet-2.id
   network_zone = "eu-central"
-  ip_range     = "10.10.200.0/24"
+  ip_range     = "10.200.0.0/24"
 }
 
 
@@ -42,12 +51,12 @@ resource "hcloud_server" "node" {
   image       = "centos-7"
   server_type = "cx11"
   network {
-    network_id = hcloud_network.network.id
-    ip         = "10.10.10.${count.index + 1}"
+    network_id = hcloud_network.network-management.id
+    ip         = "10.10.0.0.${count.index + 1}"
   }
   network {
-    network_id = hcloud_network.network.id
-    ip         = "10.10.100.${count.index + 1}"
+    network_id = hcloud_network.network-intranet-1.id
+    ip         = "10.100.0.${count.index + 1}"
   }
   depends_on = [
     hcloud_network_subnet.network-subnet-management,
